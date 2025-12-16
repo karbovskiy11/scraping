7c1b80dbe8d4332fce13f1e9c15d238de520eb0d
karbovskiy11 <karbovskiy11@gmail.com>
2025-12-16 23:00:07 +0400

    update main

--- Comissariates/main.py
+++ Comissariates/main.py
@@ -1,17 +1,23 @@
+import random
+from os import mkdir
+
 from bs4 import BeautifulSoup
 import requests
 import certifi
 import json
+import time
+import os
 
 
-def get_json(url, headers):
-    response = requests.get(url=url, headers=headers, verify=False)
-    response.raise_for_status()
+def get_json(url):
+    response = requests.get(url=url, verify=False)
+    # response.raise_for_status()
     return response.json()
 
 
-def save_data(data):
-    with open('id_data.json', 'w', encoding='utf-8') as json_file:
+def save_data(id_data, data):
+    os.makedirs('data', exist_ok=True)
+    with open(f'data/{id}_data.json', 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 
@@ -27,23 +33,44 @@ def get_id(data):
     id_comissariates = []
     for point in data:
         id_point = point['id']
-        id_comissariates.append(id_point)
+        id_comissariates.append(get_data(id_point))
 
     return id_comissariates
 
 
-def main():
-    url = 'https://mil.ru/api/ssp-maps/992a7991-05f0-433c-988f-ff28c9a1aaa8'
-    headers = {
-        'accept': 'application/json',
-        'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0'
-    }
+def get_data(data):
+    api_url = f'https://mil.ru/api/ssp-maps/point/{data}'
+    return api_url
 
-    # id_comissariates = get_json(url, headers)
-    # save_data(id_comissariates)
+
+
+
+def main():
+    api_url = 'https://mil.ru/api/ssp-maps/992a7991-05f0-433c-988f-ff28c9a1aaa8'
+    # headers = {
+    #     'accept': 'application/json',
+    #     'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0'
+    # }
+    idd = 0
+    id_comissariates = get_json(api_url)
+    save_data(idd, id_comissariates)
     id_date = load_date()
     id_comissariates = get_id(id_date)
-    print(id_comissariates)
+
+    spisok_comissariates = []
+    for item in id_comissariates:
+        point = get_json(item)
+        spisok = {
+                'type': point['data']['type'],
+                'title': point['data']['title'],
+                'info': point['data']['text_before'],
+        }
+
+        spisok_comissariates.append(spisok)
+        # save_data()
+        print(spisok)
+        time.sleep(random.randrange(2, 4))
+
 
 
 if __name__ == '__main__':
