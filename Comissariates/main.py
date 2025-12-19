2b6365513e4a254a9cf4c0c74b2a2cbb02d04930
karbovskiy11 <karbovskiy11@gmail.com>
2025-12-19 20:56:20 +0400

    update main

--- Comissariates/main.py
+++ Comissariates/main.py
@@ -1,36 +1,48 @@
+import csv
+import json
+import os
 import random
 import re
-from os import mkdir
-
-from bs4 import BeautifulSoup
-import requests
-import certifi
-import json
 import time
-import os
-import csv
+import warnings
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
 
 
 def get_json(url):
-    response = requests.get(url=url, verify=False)
-    # response.raise_for_status()
-
-    # save_data(idd, response.json())
-    return response.json()
+    try:
+        warnings.simplefilter('ignore', InsecureRequestWarning)
+        response = requests.get(url=url, verify=False, timeout=5)
+        if response.status_code == 200:
+            print(f"✓ API доступен: код {response.status_code}")
+            return response.json()
+        elif response.status_code in [401, 403]:
+            print(f"⚠ API требует аутентификации: {response.status_code}")
+        elif response.status_code == 404:
+            print(f"✗ API не найден: {response.status_code}")
+        elif response.status_code >= 500:
+            print(f"✗ Проблемы с сервером: {response.status_code}")
+        else:
+            print(f"? Неожиданный статус: {response.status_code}")
+    except requests.exceptions.ConnectionError:
+        print("✗ Не удалось подключиться к API")
+    except requests.exceptions.Timeout:
+        print("✗ Таймаут при подключении к API")
+    except requests.exceptions.RequestException as e:
+        print(f"✗ Ошибка: {e}")
 
 
 def save_json(name, data):
     os.makedirs('data', exist_ok=True)
-    with open(f'data/{name}_data.json', 'w', encoding='utf-8') as json_file:
+    with open(f'data/{name}_comissariates.json', 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 
-def load_date(name):
-    with open(f'data/{name}_data.json', 'r', encoding='utf-8') as json_file:
+def load_date():
+    with open(f'data/id_comissariates.json', 'r', encoding='utf-8') as json_file:
         id_data = json.load(json_file)
-
-    points = id_data
-    return points
+    return id_data
 
 
 def get_id(data):
@@ -46,68 +58,84 @@ def get_data(data):
     api_url = f'https://mil.ru/api/ssp-maps/point/{data}'
     return api_url
 
-def save_csv(data):
+
+def create_csv():
     with open('data/comissariates.csv', 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        writer.writerows(data)
+        writer.writerow(
+            [
+                '№',
+                'Тип',
+                'Наименование',
+                'Адрес',
+                'Телефон',
+                'Время работы'
+            ]
+        )
 
-def clean_data(data):
+
+def save_in_csv(data):
+    with open('data/comissariates.csv', 'a', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(data.values())
+
+
+def clean_symbol(data):
     clean_info = re.sub(r'<[^>]+>', '', data)
     return clean_info
 
 
 def main():
     api_url = 'https://mil.ru/api/ssp-maps/992a7991-05f0-433c-988f-ff28c9a1aaa8'
+    if get_json(api_url):
+        id_comissariates = get_json(api_url)
+        name = 'id'
+        save_json(name, id_comissariates['data']['points'])
+        id_date = load_date()
+        id_comissariates = get_id(id_date)
+        create_csv()
+        spisok_comissariates = []
 
-    id_comissariates = get_json(api_url)
-    name = 'id'
-    save_json(name, id_comissariates['data']['points'])
-    id_date = load_date(name)
-    id_comissariates = get_id(id_date)
-
-    spisok_comissariates = []
-
-    count = 0
-    for item in id_comissariates:
-        if count < 4:
+        count = 1
+        for item in id_comissariates:
+        # if count < 4:
             point = get_json(item)
-            # spisok_json = {
-            #         'type': point['data']['type'],
-            #         'title': point['data']['title'],
-            #         'info': point['data']['text_before'],
-            # }
             data_type = point['data']['type']
             data_title = point['data']['title']
-
-            data_info = [clean_data(i) for i in point['data']['text_before'].split('<br>')]
+            data_info = [clean_symbol(i) for i in point['data']['text_before'].split('<br>') if i != '']
+            try:
+                data_address = data_info[0]
+                # print(f'{data_address}')
+            except IndexError:
+                data_address = 'Информация отсутствует'
+            try:
+                data_telefon = data_info[1]
+                # print(f'{data_telefon}')
+            except IndexError:
+                data_telefon = 'Информация отсутствует'
             try:
                 data_time = data_info[2]
-            except:
+                # print(f'{data_time}')
+            except IndexError:
                 data_time = 'Информация отсутствует'
-            print(data_info)
-            # data_info = clean_data(data_info)
-            spisok = [
-                    data_type,
-                    data_title,
-                    data_info[0],
-                    data_info[1],
-                    data_time
-                    # clean_data(point['data']['text_before'].split('<br>'))
-
-            ]
-
+            spisok = {
+                '№': count,
+                'Тип': data_type,
+                'Наименование': data_title,
+                'Адрес': data_address.replace('Адрес: ', ''),
+                'Телефон': data_telefon.replace('Телефон: ', ''),
+                'Время работы': data_time.replace('Время работы: ', ''),
+            }
             spisok_comissariates.append(spisok)
-
-            print(spisok)
+            print(f'Информация о военкомате № {count} собрана.')
+            save_in_csv(spisok)
+            name = 'spisok'
             count += 1
             time.sleep(random.randrange(2, 4))
-        else:
-            break
+        # else:
+        #     break
 
-
-    name = 'spisok'
-    save_json(name, spisok_comissariates)
-    save_csv(spisok_comissariates)
+        save_json(name, spisok_comissariates)
 
 
 if __name__ == '__main__':
